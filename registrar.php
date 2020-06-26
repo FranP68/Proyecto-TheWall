344b2f51c3319f06d37921b39c10b62141bf3a7d
Francisco Pavon <franciscopavon68@gmail.com>
2020-06-26 20:42:08 -0300

    empezando a validar los datos de registrar usuario

--- registrar.php
+++ registrar.php
@@ -1,14 +1,13 @@
 <?php
     require 'BD.php';
-    if ((!empty($_POST['apellidos'])) && (!empty($_POST['nombre'])) && (!empty($_POST['clave']))){
+    if (   (  !empty($_POST['apellidos']) )  && (  !empty($_POST['nombre'])  ) && (  !empty($_POST['clave']) )  &&  (  !empty($_POST['clave2']) )  &&  (  !empty($_POST['usuario']) ) &&  (  !empty($_POST['correo']) ) &&  (  !empty($_FILES['img']['name']) ) )
+    {
         $apellido = $_POST['apellidos'];
         $nombre = $_POST['nombre'];
         $clave =  $_POST['clave'];
         $clave2 =  $_POST['clave2'];
         $email =  $_POST['correo'];
         $nombreUsuario =  $_POST['usuario'];
-        $folder = 'images';
-        $imagenName = $_FILES['img']['name'];
         $imagenTmp = $_FILES['img']['tmp_name'];
         $imagenType = $_FILES['img']['type'];
         $bytesImagen = addslashes(file_get_contents($imagenTmp));
