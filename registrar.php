efbba465b1f8fcfd360746b87945d5a22522427f
Francisco Pavon <franciscopavon68@gmail.com>
2020-06-27 01:59:13 -0300

    terminando el registrar con verificacion

--- registrar.php
+++ registrar.php
@@ -1,7 +1,8 @@
 <?php
     require 'BD.php';
     if (   (  !empty($_POST['apellidos']) )  && (  !empty($_POST['nombre'])  ) && (  !empty($_POST['clave']) )  &&  (  !empty($_POST['clave2']) )  &&  (  !empty($_POST['usuario']) ) &&  (  !empty($_POST['correo']) ) &&  (  !empty($_FILES['img']['name']) ) )
-    {
+    {   
+        //obtengo datos del formulario
         $apellido = $_POST['apellidos'];
         $nombre = $_POST['nombre'];
         $clave =  $_POST['clave'];
@@ -12,16 +13,55 @@
         $imagenType = $_FILES['img']['type'];
         $bytesImagen = addslashes(file_get_contents($imagenTmp));
         $tipo=substr($imagenType, 6);
-        
-        
-        $sql1 = "INSERT INTO usuarios(nombre,apellido, email,foto_tipo, nombreUsuario,foto_contenido, contrasenia) VALUES ('$nombre','$apellido','$email','$tipo','$nombreUsuario','$bytesImagen', '$clave') "; 
-        if (mysqli_query($conn, $sql1)) {
-            echo "Nuevo registro.";
+        //---------------
+
+        usuario_duplicado($nombreUsuario);//devuelve true si esta duplicado
+        $error_claveDif=false;
+        $claveOK=false;
+        if($clave == $clave2){
+            if( validar_clave( $clave, $error_clave )) {
+                $claveOK=true ;
+            }
+            else {
+                echo $error_clave;
+            }
         }
         else{
-            echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
+            echo " Las contraseñas son diferentes " . "<br>";
+            $error_claveDif=true;
+        }
+        $expresionSoloLetras = " /^[a-z]+$/i ";
+        $bienNombre=true;
+        if (!preg_match($expresionSoloLetras, $nombre)) {
+            echo "El nombre deben ser solo letras " . "<br>";
+            $bienNombre=false;
+        }
+        $bienApellido=true;
+        if (!preg_match($expresionSoloLetras, $apellido)) {
+            echo "El apellido deben ser solo letras " . "<br>";
+            $bienApellido=false;
         }
         
+         
+        if(!usuario_duplicado($nombreUsuario) && $bienNombre  && !$error_claveDif && $claveOK && $bienApellido){
+            //agrego nuevo usuario a la base de datos
+            $sql1 = "INSERT INTO usuarios(nombre,apellido, email,foto_tipo, nombreUsuario,foto_contenido, contrasenia) VALUES ('$nombre','$apellido','$email','$tipo','$nombreUsuario','$bytesImagen', '$clave') ";
+            if (mysqli_query($conn, $sql1)) {
+                echo "Nuevo registro.";
+                
+            }
+            else{
+                echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
+            }
+        }else{
+            echo "El nombre de usuario ya esta en uso";
+        }    
+    }
+    else{
+        if(empty($_FILES['img']['name'])){
+            echo "La foto de perfil no está definida";
+        }
+
     }
     //probando foto
     /*
@@ -36,3 +76,49 @@
     <?php } ?>
 */
 ?>    
+<?php
+
+function usuario_duplicado($nombreUsuario){//devuelve true si esta duplicado
+    require 'BD.php';
+    $sql= "SELECT nombreusuario FROM usuarios WHERE (nombreusuario = '$nombreUsuario') ";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        //echo "El nombre de usuario ya esta en uso";
+        return true;
+    }
+    else {
+        //echo "El nombre de usuario esta disponible";
+        return false;
+    }
+} 
+
+    
+?>
+
+
+<?php
+function validar_clave($clave,&$error_clave){
+    if(strlen($clave) < 6){
+       $error_clave = "La clave debe tener al menos 6 caracteres" ."<br>";
+       return false;
+    }
+    if(strlen($clave) > 16){
+       $error_clave = "La clave no puede tener más de 16 caracteres" ."<br>";
+       return false;
+    }
+    if (!preg_match('[a-z]',$clave)){
+       $error_clave = "La clave debe tener al menos una letra minúscula" ."<br>";
+       return false;
+    }
+    if (!preg_match('[A-Z]',$clave)){
+       $error_clave = "La clave debe tener al menos una letra mayúscula " ."<br>";
+       return false;
+    }
+    if (!preg_match('[0-9]',$clave)){
+       $error_clave = "La clave debe tener al menos un caracter numérico" ."<br>";
+       return false;
+    }
+    $error_clave = "";
+    return true;
+ }
+?>
