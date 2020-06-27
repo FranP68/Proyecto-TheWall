c51e8d41a0d53c0b0cd932b2c40cb342ac8c0283
Francisco Pavon <franciscopavon68@gmail.com>
2020-06-27 20:11:24 -0300

    Nuevo Mensaje terminado

--- nuevoMensaje.php
+++ nuevoMensaje.php
@@ -1,24 +1,30 @@
 <?php
-    require 'BD.php';
-    session_start();
+require 'BD.php';
+session_start();
+if  (   (!empty($_POST['nuevoMensaje']))     )  {
+    
     $usuario = $_SESSION['usuario'];
     $nombre = $_SESSION['nombre'];
     $apellido = $_SESSION['apellido'];
-    
+    $idUsuario = $_SESSION['id'];
     $texto = $_POST['nuevoMensaje'];
-    $imagenTmp = $_FILES['img']['tmp_name'];
-    $imagenType = $_FILES['img']['type'];
-    $bytesImagen = addslashes(file_get_contents($imagenTmp));
-    $tipo=substr($imagenType, 6);
-    $rs=mysqli_query($conn, "SELECT id FROM usuarios WHERE nombreusuario='$usuario' ");
-    if ($row = mysqli_fetch_row($rs)) {
-        $id = trim($row[0]);
-    }
-    $sql1 = "INSERT INTO mensaje(texto, imagen_contenido, imagen_tipo, usuarios_id, fechayhora) VALUES ('$texto', '$bytesImagen' , '$tipo' , '$id' , 'current_Date()') ";
-    if (mysqli_query($conn, $sql1)) {
-        echo "Nuevo mensaje.";
+    date_default_timezone_set("America/Argentina/Buenos_Aires");
+    $hoy = date('Y-m-d H:i:s');
+    
+    if (!empty($_FILES['img']['name'])) {
+        $imagenTmp = $_FILES['img']['tmp_name'];
+        $imagenType = $_FILES['img']['type'];
+        $bytesImagen = addslashes(file_get_contents($imagenTmp));
+        $tipo = substr($imagenType, 6);
+        $sql1 = "INSERT INTO mensaje(texto, imagen_contenido, imagen_tipo, usuarios_id, fechayhora) VALUES ('$texto', '$bytesImagen' , '$tipo' , '$idUsuario' , '$hoy') ";    
     }
     else{
+        $sql1 = "INSERT INTO mensaje(texto, usuarios_id, fechayhora) VALUES ('$texto', '$idUsuario' , '$hoy') ";
+    }
+    
+    if (mysqli_query($conn, $sql1)) {
+        echo "Nuevo mensaje.";
+    } else {
         echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
     }
-?>
+}
--- registrar.php
+++ registrar.php
@@ -42,13 +42,20 @@
                 $_SESSION['usuario']=$nombreUsuario;
                 $_SESSION['nombre']=$nombre;
                 $_SESSION['apellido']=$apellido;
-                
+                //obtengo id de usuario
+                $rs=mysqli_query($conn, "SELECT id FROM usuarios WHERE nombreusuario='$nombreUsuario' ");
+                if ($row = mysqli_fetch_row($rs)) {
+                    $id = trim($row[0]);
+                }
+                $_SESSION['id']=$id;
                 /* $rs = mysqli_query ($conn, " SELECT MAX(id_tabla) AS id FROM tabla " );
                 if ($row = mysqli_fetch_row($rs)) {
                     $id = trim($row[0]);
                     
                 }
+                
                 $_SESSION['id']=$id; */
+
                 header("Location:index.php");
                 
             }
