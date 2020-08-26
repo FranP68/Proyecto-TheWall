a83586f7e91d52340c30f11b748d61675621cb70
san60661 <san60661@gmail.com>
2020-08-26 18:59:46 -0300

    correccion EditarPerfil (redireccion y muestra de mensajes con validacion)

--- VeditarPerfil.php
+++ VeditarPerfil.php
@@ -1,108 +1,113 @@
 <?php
-    include 'claseVerificar.php';
-    require 'BD.php';
 
-    session_start();
-    $usuario = $_SESSION['usuario'];
-    $emailActual = $_SESSION['email'];
 
-    if (   (  !empty($_POST['apellidos']) )  && (  !empty($_POST['nombre'])  )  &&   (  !empty($_POST['correo']) ) ) 
-    { 
-        $apellido = $_POST['apellidos'];
-        $nombre = $_POST['nombre'];
-        $email =  $_POST['correo'];
+    // include 'claseVerificar.php';
+    // require 'BD.php';
+
+    // session_start();
+    // $usuario = $_SESSION['usuario'];
+    // $emailActual = $_SESSION['email'];
+    // $alert ="";
+    // if (isset($_POST['submit'])){
+    // if (   (  !empty($_POST['apellidos']) )  && (  !empty($_POST['nombre'])  )  &&   (  !empty($_POST['correo']) ) ) 
+    // { 
+    //     $apellido = $_POST['apellidos'];
+    //     $nombre = $_POST['nombre'];
+    //     $email =  $_POST['correo'];
         
 
        
-        // verificar nombre
-        $nombreOk=Verificar::validar_nombre($nombre,$error_nombre);
+    //     // verificar nombre
+    //     $nombreOk=Verificar::validar_nombre($nombre,$error_nombre);
 
-        // ---------------------
+    //     // ---------------------
 
-        // verificar apellido
+    //     // verificar apellido
 
-        $apellidoOk = Verificar::validar_apellido($apellido,$error_apellido); 
+    //     $apellidoOk = Verificar::validar_apellido($apellido,$error_apellido); 
 
-        // ---------------------
+    //     // ---------------------
 
-        // verificar email
+    //     // verificar email
 
-        $emailOk = Verificar::validar_email($email, $error_email);
+    //     $emailOk = Verificar::validar_email($email, $error_email);
 
-        // ---------------------
+    //     // ---------------------
 
-        if($email==$emailActual){
-            $emailDuplicadoOk=true;
-        }
-        else{
-            $emailDuplicadoOk = Verificar::email_duplicado($email,$error_emailDuplicado);
-        }
+    //     if($email==$emailActual){
+    //         $emailDuplicadoOk=true;
+    //     }
+    //     else{
+    //         $emailDuplicadoOk = Verificar::email_duplicado($email,$error_emailDuplicado);
+    //     }
         
 
-        if( $nombreOk && $emailOk && $apellidoOk && $emailDuplicadoOk){
-            //actualizo usuario en la base de datos
+    //     if( $nombreOk && $emailOk && $apellidoOk && $emailDuplicadoOk){
+    //         //actualizo usuario en la base de datos
             
-            if(empty($_FILES['img']['name']) ){
-                $sql1 = "UPDATE usuarios SET nombre='$nombre', apellido='$apellido' , email='$email'  WHERE nombreusuario='$usuario' ";
-            }
-            elseif(!empty($_FILES['img']['name']) ){
+    //         if(empty($_FILES['img']['name']) ){
+    //             $sql1 = "UPDATE usuarios SET nombre='$nombre', apellido='$apellido' , email='$email'  WHERE nombreusuario='$usuario' ";
+    //         }
+    //         elseif(!empty($_FILES['img']['name']) ){
                 
-                $imagenTmp = $_FILES['img']['tmp_name'];
-                $imagenType = $_FILES['img']['type'];
-                $bytesImagen = addslashes(file_get_contents($imagenTmp));
-                $tipo=substr($imagenType, 6);
-                if (Verificar::validar_foto($tipo,$error_foto)){             
-                    $sql1 = "UPDATE usuarios SET nombre='$nombre', apellido='$apellido' , email='$email', foto_contenido='$bytesImagen', foto_tipo='$tipo'  WHERE nombreusuario='$usuario' ";
-                }
-                else{
-                    $sql1 = "UPDATE usuarios SET nombre='$nombre', apellido='$apellido' , email='$email'  WHERE nombreusuario='$usuario' ";
-                    echo $error_foto;
-                }
-            }
+    //             $imagenTmp = $_FILES['img']['tmp_name'];
+    //             $imagenType = $_FILES['img']['type'];
+    //             $bytesImagen = addslashes(file_get_contents($imagenTmp));
+    //             $tipo=substr($imagenType, 6);
+    //             if (Verificar::validar_foto($tipo,$error_foto)){             
+    //                 $sql1 = "UPDATE usuarios SET nombre='$nombre', apellido='$apellido' , email='$email', foto_contenido='$bytesImagen', foto_tipo='$tipo'  WHERE nombreusuario='$usuario' ";
+    //             }
+    //             else{
+    //                 $sql1 = "UPDATE usuarios SET nombre='$nombre', apellido='$apellido' , email='$email'  WHERE nombreusuario='$usuario' ";
+    //                 // echo $error_foto;
+    //             }
+    //         }
             
-            if (mysqli_query($conn, $sql1)) {
-                echo "Actualizar registro.";
-                $_SESSION['nombre'] = $nombre;
-                $_SESSION['apellido'] =$apellido; 
-                $_SESSION['correo'] = $email; 
-        
-                header("Location:inicio.php");
+    //         if (mysqli_query($conn, $sql1)) {
+    //             $alert = "Datos actualizados";
+    //             $_SESSION['nombre'] = $nombre;
+    //             $_SESSION['apellido'] =$apellido; 
+    //             $_SESSION['correo'] = $email; 
                 
-            }
-            else{
-                echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
-            }
-        }else{
-            if (!$emailDuplicadoOk)
-                echo "$error_emailDuplicado";
-            if (!$emailOk)
-                echo "$error_email";
-            if (!$nombreOk)
-                echo "$error_nombre";
-            if (!$apellidoOk)
-                echo "$error_apellido";
-            header("Location:inicio.php"); //comentar esta linea si quiero ver las verificaciones de php
-           // echo "El nombre de usuario ya esta en uso 11";
-        }    
-        }
+    //             $bool= true;
+    //             header("Location:miPerfil.php");
+                
+    //         }
+    //         else{
+    //             //echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
+    //         }
+    //     }else{
+    //         if (!$emailDuplicadoOk)
+    //             $alert = $alert . "$error_emailDuplicado";
+    //         if (!$emailOk)
+    //              $alert = $alert . "$error_email";
+                
+    //         if (!$nombreOk)
+    //             $alert = $alert . "$error_nombre";
+                
+    //         if (!$apellidoOk)
+    //             $alert = $alert . "$error_apellido";
+    //     }    
+    //     }
 
 
 
     
-    else{
-        if(  empty($_POST['apellidos']) ){
-            echo "El apellido no está definido"."<br>";
-        }
-        if(  empty($_POST['nombre']) ){
-            echo "El nombre no está definido"."<br>";
-        }
-        if(  empty($_POST['correo']) ){
-            echo "El correo no está definido"."<br>";
-        }
-        if(  empty($_FILES['img']['name'])){
-            echo "El imagen no está definida"."<br>";
-        }
-        header("Location:inicio.php"); //comentar esta linea si quiero ver las verificaciones de php
-    }
+    // else{
+    //     if(  empty($_POST['apellidos']) ){
+    //         $alert = $alert . "El apellido no está definido"."<br>";
+    //     }
+    //     if(  empty($_POST['nombre']) ){
+    //         $alert = $alert . "El nombre no está definido"."<br>";
+    //     }
+    //     if(  empty($_POST['correo']) ){
+    //         $alert= $alert . "El correo no está definido"."<br>";
+    //     }
+       
+    // }
+    // //header("Location:editarPerfil.php");
+    //     $bool = false;
+    // }
+
 
 ?>
--- editarPerfil.php
+++ editarPerfil.php
@@ -1,6 +1,125 @@
 <!doctype html>
+
+
 <?php
-  session_start();
+
+
+    include 'claseVerificar.php';
+    require 'BD.php';
+
+    session_start();
+    $usuario = $_SESSION['usuario'];
+    $emailActual = $_SESSION['email'];
+    $alert ="";
+    if (isset($_POST['submit'])){
+    if (   (  !empty($_POST['apellidos']) )  && (  !empty($_POST['nombre'])  )  &&   (  !empty($_POST['correo']) ) ) 
+    { 
+        $apellido = $_POST['apellidos'];
+        $nombre = $_POST['nombre'];
+        $email =  $_POST['correo'];
+        
+
+       
+        // verificar nombre
+        $nombreOk=Verificar::validar_nombre($nombre,$error_nombre);
+
+        // ---------------------
+
+        // verificar apellido
+
+        $apellidoOk = Verificar::validar_apellido($apellido,$error_apellido); 
+
+        // ---------------------
+
+        // verificar email
+
+        $emailOk = Verificar::validar_email($email, $error_email);
+
+        // ---------------------
+
+        if($email==$emailActual){
+            $emailDuplicadoOk=true;
+        }
+        else{
+            $emailDuplicadoOk = Verificar::email_duplicado($email,$error_emailDuplicado);
+        }
+        
+
+        if( $nombreOk && $emailOk && $apellidoOk && $emailDuplicadoOk){
+            //actualizo usuario en la base de datos
+            
+            if(empty($_FILES['img']['name']) ){
+                $sql1 = "UPDATE usuarios SET nombre='$nombre', apellido='$apellido' , email='$email'  WHERE nombreusuario='$usuario' ";
+            }
+            elseif(!empty($_FILES['img']['name']) ){
+                
+                $imagenTmp = $_FILES['img']['tmp_name'];
+                $imagenType = $_FILES['img']['type'];
+                $bytesImagen = addslashes(file_get_contents($imagenTmp));
+                $tipo=substr($imagenType, 6);
+                if (Verificar::validar_foto($tipo,$error_foto)){             
+                    $sql1 = "UPDATE usuarios SET nombre='$nombre', apellido='$apellido' , email='$email', foto_contenido='$bytesImagen', foto_tipo='$tipo'  WHERE nombreusuario='$usuario' ";
+                }
+                else{
+                    $sql1 = "UPDATE usuarios SET nombre='$nombre', apellido='$apellido' , email='$email'  WHERE nombreusuario='$usuario' ";
+                    // echo $error_foto;
+                }
+            }
+            
+            if (mysqli_query($conn, $sql1)) {
+                $alert = "Datos actualizados";
+                $_SESSION['nombre'] = $nombre;
+                $_SESSION['apellido'] =$apellido; 
+                $_SESSION['correo'] = $email; 
+                
+                $bool= true;
+                header("Location:miPerfil.php");
+                
+            }
+            else{
+                //echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
+            }
+        }else{
+            if (!$emailDuplicadoOk)
+                $alert = $alert . "$error_emailDuplicado";
+            if (!$emailOk)
+                 $alert = $alert . "$error_email";
+                
+            if (!$nombreOk)
+                $alert = $alert . "$error_nombre";
+                
+            if (!$apellidoOk)
+                $alert = $alert . "$error_apellido";
+        }    
+        }
+
+
+
+    
+    else{
+        if(  empty($_POST['apellidos']) ){
+            $alert = $alert . "El apellido no está definido"."<br>";
+        }
+        if(  empty($_POST['nombre']) ){
+            $alert = $alert . "El nombre no está definido"."<br>";
+        }
+        if(  empty($_POST['correo']) ){
+            $alert= $alert . "El correo no está definido"."<br>";
+        }
+       
+    }
+    //header("Location:editarPerfil.php");
+        $bool = false;
+    }
+
+
+?>
+
+
+
+
+<?php
+  
   error_reporting(0);
   $s=$_SESSION['usuario'];
   
@@ -42,7 +161,7 @@
     
   <?php
       require "BD.php";
-      session_start();
+      
       $usuario = $_SESSION['usuario'];
       $nombre = $_SESSION['nombre'];
       $apellido = $_SESSION['apellido'];
@@ -91,12 +210,12 @@
   
   <img src="data:image/jpeg; base64, <?php echo base64_encode($bytesImagen) ?> " class="avatar2" alt="">
             </div>
-    <form  class="col-12" action="VeditarPerfil.php"  method="POST" class="form-register" enctype="multipart/form-data" onsubmit="return validarEditarPerfil(img);">
+    <form  class="col-12"  method="POST" class="form-register" enctype="multipart/form-data" onsubmit="return validarEditarPerfil(img);">
         <div class="form-group" id="user-group"></div>
         <h2 class="form__titulo">Editar Perfil</h2>
         <div class="contenedor-inputs">
             <div class="form-group" id="user-group">
-            <input type="text" id="nombre" name="nombre" value="<?PHP echo $nombre ?>" class="form-control" >
+            <input type="text" id="nombre" name="nombre" value="<?PHP echo $_SESSION['nombre'] ?>" class="form-control" >
         </div>
         <div class="form-group" id="user-group">
             <input type="text" id="apellidos" name="apellidos" value="<?PHP echo $apellido ?>" class="form-control"  >
@@ -111,10 +230,22 @@
         </div>
             
         </div>
-        <button type="submit" name="submit" class="btn btn-primary"><i class="fas fa-sign-in-alt"></i>  Guardar Cambios</button>
-    
-                                                                                                                                                                                       
+        <button type="submit" name="submit" onclick="preguntar2()" class="btn btn-primary"><i class="fas fa-sign-in-alt"></i>  Guardar Cambios</button>
+        <script type="text/javascript">
+                    function preguntar2(){
+                      if ( confirm( "¿Esta seguro que desea actualizar sus datos?" ) ){
+                        // window.location.href = "http://localhost/ProyectoPHP/Proyecto-TheWall/miPerfil.php";
+                      }
+                      
+                    }
+        </script>    
+        <?php 
+             echo " <p> $alert </p>" ;
+        ?> 
+        
+
       </form>
+      
 </main>
     
 
@@ -133,3 +264,4 @@
 
 
 </html>
+
